9121f5c667622edbb963b0df930d968b46119f34
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2016-06-26 14:53:22 -0700

    add some styling to room list on index

--- app/helpers/room-popularity.js
+++ app/helpers/room-popularity.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function roomPopularity(room) {
   var room = room[0];
   if (room.get('reviews').get('length') >=3 ){
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire glyphicon-fire-red"></span>');
   }
 }
 export default Ember.Helper.helper(roomPopularity);
