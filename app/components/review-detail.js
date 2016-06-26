46bf1112c2aff5700c64195df8cb89ecdb0a6b5b
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2016-06-26 13:29:39 -0700

    add work in progress to display content on read route as well as sortedReviews on room route

--- app/components/review-detail.js
+++ app/components/review-detail.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  fullAddress: Ember.computed('room.address', 'room.city', function() {
+      return this.get('room.address') + ', ' + this.get('room.city');
+      debugger;
+    }),
+
   sortBy: ['ranting:asc'],
   sortedReviews: Ember.computed.sort('room.reviews', 'sortBy'),
 
@@ -9,7 +14,7 @@ export default Ember.Component.extend({
     deleteReview(currentReview) {
       if (confirm('Are you sure you want to delete this review?')) {
         this.sendAction('destroyReviewCp', currentReview);
+        }
       }
     }
-  }
 });
--- app/components/review-tile.js
+++ app/components/review-tile.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   readLater: Ember.inject.service(),
 
-  actions: {
-    addToReadLater(review) {
-      this.get('readLater').add(review);
+    actions: {
+      addToReadLater(review) {
+        this.get('readLater').add(review);
+      }
     }
-  }
 });
